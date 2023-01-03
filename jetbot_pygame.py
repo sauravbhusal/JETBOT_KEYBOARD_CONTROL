4cafb2b471d9c44f197840b4369f3ff25f772b4a
Saurav Bhusal <bhusalsaurav0@gmail.com>
2023-01-03 22:14:41 +0545

    display_function_of_specific_key

--- jetbot_pygame.py
+++ jetbot_pygame.py
@@ -17,14 +17,17 @@ while True:
 
             if event.key == pygame.K_w:
                 keypress = 8
+                print ("keypress = forward")
 
             if event.key == pygame.K_s:
                 keypress = 2
+                print ("keypress = backward")
 
             if event.key == pygame.K_a:
                 keypress = 4
+                print ("keypress = left")
 
             if event.key == pygame.K_d:
                 keypress = 6
-                
+                print ("keypress = right") 
                                
