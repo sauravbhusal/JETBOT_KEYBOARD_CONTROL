52fa33e8155e061fcd2fbd37255107934c7d856b
Saurav Bhusal <bhusalsaurav0@gmail.com>
2023-01-05 13:47:14 +0545

    client_and_jetbot_control_code

---
+++ jetbot_client.py
@@ -0,0 +1,18 @@
+while True:
+    res = requests.get('ip_address_where_server_streamed_the_value')
+    print ('val = ',res.text)
+    
+    if res.text == '8':
+        robot.backward(0.2)
+        
+    elif res.text == '4':
+        robot.left(0.2)
+        
+    elif res.text == '6':
+        robot.right(0.2)
+        
+    elif res.text == '2':
+        robot.forward(0.2)
+        
+    elif res.text == '0':
+        robot.stop()
